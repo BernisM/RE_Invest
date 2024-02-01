36fdbd6cb9affe82830f73c2401e2d1f66735a04
BernisM <hellobernis@outlook.com>
2024-02-01 21:06:56 +0100

    test

--- Scraping_Last_Sale_Price_v2.py
+++ Scraping_Last_Sale_Price_v2.py
@@ -1,19 +1,33 @@
-import os
-import requests
+from bs4 import BeautifulSoup
+import csv
+import pandas as pd
 
-url = "https://www.meilleursagents.com/prix-immobilier/dvf/grenoble-38000/avenue-rhin-et-danube-1131337117/26/"
-output_directory = r"C:\Users\massw\OneDrive\Bureau\Programmation\RE_Invest\RE_Invest"
-output_filename = "page.html"
+# Charger le fichier HTML
+html_file_path = "C:/Users/massw/OneDrive/Bureau/Programmation/RE_Invest/RE_Invest/26_rue_rhin_et_danube.txt"
+save_name = "C:/Users/massw/OneDrive/Bureau/Programmation/RE_Invest/RE_Invest/26_rue_rhin_et_danube.csv"
 
-response = requests.get(url)
+# Charger le fichier HTML 
+with open(html_file_path, 'r', encoding='utf-8') as file: 
+    html_content = file.read() 
 
-if response.status_code == 200:
-    html_content = response.text
-    output_path = os.path.join(output_directory, output_filename)
+print(html_content)    
 
-    with open(output_path, "w", encoding="utf-8") as file:
-        file.write(html_content)
-
-    print(f"Enregistrée sous : {output_path}")
-else:
-    print(f"Error --> Code d'état HTTP : {response.status_code}")
+# Utiliser BeautifulSoup pour extraire les données 
+soup = BeautifulSoup(html_content, 'html.parser') 
+    
+# Extraire les valeurs nécessaires 
+addresses = [div.text.strip() for div in soup.find_all('div', class_='dvf-transaction__address')] 
+features = [span.text.strip() for span in soup.find_all('span', class_='dvf-transaction__feature')] 
+area_values = [span['data-dvf-transaction-area'] for span in soup.find_all('span', class_='dvf-transaction__feature', 'data-dvf-transaction-area': True)] 
+price_dates = [span.text.strip() for span in soup.find_all('span', class_='dvf-transaction__price-date')] 
+price_values = [span['data-dvf-transaction-price-value'] for span in soup.find_all('span', class_='dvf-transaction__price-value', {'data-dvf-transaction-price-value': True})] 
+    
+# Créer un DataFrame pandas 
+df = pd.DataFrame({ 'Address': addresses, 
+                       'Feature': features, 
+                       'Area Value': area_values, 
+                       'Price Date': price_dates, 
+                       'Price Value': price_values }) 
+    
+# Sauvegarder dans un fichier CSV 
+df.to_csv(save_name, index=False)
